61ce83d977b46085c00eaa1a065c4e7b6b4d0824
Vu Ngoc Tuan <vungoctuan203@gmail.com>
2018-02-26 10:35:13 +0700

    Code logic for login function

--- app/src/main/java/com/training/vungoctuan/mvpdemo/activity/login/LoginActivity.java
+++ app/src/main/java/com/training/vungoctuan/mvpdemo/activity/login/LoginActivity.java
@@ -1,16 +1,77 @@
 package com.training.vungoctuan.mvpdemo.activity.login;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.training.vungoctuan.mvpdemo.MvpApp;
+import com.training.vungoctuan.mvpdemo.R;
+import com.training.vungoctuan.mvpdemo.activity.main.MainActivity;
+import com.training.vungoctuan.mvpdemo.model.DataManager;
+import com.training.vungoctuan.mvpdemo.utils.CommonUtils;
 import com.training.vungoctuan.mvpdemo.view.BaseActivity;
 
 /**
  * Created by vungoctuan on 2/26/18.
  */
 public class LoginActivity extends BaseActivity implements LoginMvpView{
+    LoginPresenter mLoginPresenter;
+    EditText mEditTextEmail, getmEditTextPassword;
+    Button mButton;
+
+    public static Intent getStartIntent(Context context) {
+        return new Intent(context, LoginActivity.class);
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        DataManager dataManager = ((MvpApp) getApplication()).getmDataManager();
+        mLoginPresenter = new LoginPresenter(dataManager);
+        mLoginPresenter.onAttach(this);
+
+        mEditTextEmail = findViewById(R.id.editTextEmail);
+        getmEditTextPassword = findViewById(R.id.editTextPassword);
+        mButton = findViewById(R.id.button);
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onLoginButtonClick();
+            }
+        });
+
+
+    }
+
     @Override
     public void openMainActivity() {
+        Intent intent = MainActivity.getStartIntent(this);
+        startActivity(intent);
+        finish();
     }
 
     @Override
     public void onLoginButtonClick() {
+        String emailId = mEditTextEmail.getText().toString();
+        String password = getmEditTextPassword.getText().toString();
+
+        if (!CommonUtils.isEmailValid(emailId)) {
+            Toast.makeText(this, "Enter correct Email", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        if (password == null || password.isEmpty()) {
+            Toast.makeText(this, "Enter Password", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        mLoginPresenter.startLogin(emailId);
     }
 }
--- app/src/main/java/com/training/vungoctuan/mvpdemo/activity/main/MainActivity.java
+++ app/src/main/java/com/training/vungoctuan/mvpdemo/activity/main/MainActivity.java
@@ -1,9 +1,16 @@
 package com.training.vungoctuan.mvpdemo.activity.main;
 
+import android.content.Context;
+import android.content.Intent;
+
+import com.training.vungoctuan.mvpdemo.activity.splash.SplashActivity;
 import com.training.vungoctuan.mvpdemo.view.BaseActivity;
 
 /**
  * Created by vungoctuan on 2/26/18.
  */
 public class MainActivity extends BaseActivity{
+    public static Intent getStartIntent(Context context) {
+        return new Intent(context, MainActivity.class);
+    }
 }
