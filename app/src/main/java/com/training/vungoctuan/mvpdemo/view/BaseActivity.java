7e59d8ce6e1b2d307dc3404680c3fa020b6a3ed8
Vu Ngoc Tuan <vungoctuan203@gmail.com>
2018-02-26 08:37:21 +0700

    Init view interface and baseactivity

---
+++ app/src/main/java/com/training/vungoctuan/mvpdemo/view/BaseActivity.java
@@ -0,0 +1,9 @@
+package com.training.vungoctuan.mvpdemo.view;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by vungoctuan on 2/26/18.
+ */
+public class BaseActivity extends AppCompatActivity implements MvpView{
+}
